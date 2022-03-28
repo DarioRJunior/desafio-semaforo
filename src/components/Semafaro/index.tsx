928f878397ebc06af18c65c08d8359938de43fae
Dario Antonio Ribeiro Junior <darioarjr321@gmail.com>
2022-03-28 14:43:43 -0300

    first commit

---
+++ src/components/Semafaro/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import './style.css';
+
+export const Semaforo = () => {
+    const [selected, setSelected] = useState<"vermelho" | "amarelo"| "verde">();
+
+    return (
+        <div className="container">
+            <div className="semafaro">
+                <button className={`vermelho ${selected === "vermelho" ? "selected" : ""}`} onClick={() => setSelected("vermelho")}/>
+                <button className={`amarelo ${selected === "amarelo" ? "selected" : ""}`} onClick={() => setSelected("amarelo")}/>
+                <button className={`verde ${selected === "verde" ? "selected" : ""}`} onClick={() => setSelected("verde")}/>
+            </div>
+            <div className="info">
+                {selected === 'vermelho' && <span>Cuidado! Espere o sinal abrir!</span>}
+                {selected === 'amarelo' && <span>Atenção, irá fechar em breve!</span>}
+                {selected === 'verde' && <span>Pode passar!</span>}
+            </div>
+        </div>
+    )
+}
